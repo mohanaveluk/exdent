15322fbfb30b84a547ab30801698c46e35df6e5d
mohanaveluk <mohanaveluk@gmail.com>
2019-09-04 16:48:28 -0500

    updating index file

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     if(req.url =='/'){
       res.end('<h1>Home page</home>');
     }
     if(req.url =='/about'){
-      res.end('<h1>About us page</home>');
+      fs.readFile(
+          path.join(__dirname, 'public', 'about.html'), (err, content) => {
+              if(err) throw err;
+              res.writeHead(200, {'content-type': 'text/html'});
+              res.end(content);
+          }
+      );
     }
   
   
